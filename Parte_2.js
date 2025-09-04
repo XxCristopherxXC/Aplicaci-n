5cc22cece39459831a751a717e794748aec178d3
XxCristopherxXC <cr.barrueto@duocuc.cl>
2025-09-04 19:50:50 -0400

    Update Parte_2.js

--- Parte_2.js
+++ Parte_2.js
@@ -1,13 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('form');
 
-    form.addEventListener('submit', function (e) {
-        const password = document.getElementById('password').value;
-        const confirmPassword = document.getElementById('confirm-password').value;
-
-        if (password !== confirmPassword) {
-            e.preventDefault();
-            alert('Las contraseñas no coinciden. Por favor, verifica.');
-        }
-    });
-});
